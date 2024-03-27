48228b0b8fa9bbd8c38d3bd039e7673c5fe39d17
YSPark <parkite576@gmail.com>
2024-03-27 22:46:24 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # this_is_for_you
-This is for you
+### This is for you
+
+### Formular    
+<hr>
+
+$x^{2/3} + e/3 * ((pi - x^{2})^{1/2}) * sin(a * pi * x)$
+
+### video    
+<hr>
+<video controls src="heart.mp4" />
---
+++ THIS_IS_FOR_YOU.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+def f(x, a):
+    return (x**(2/3) + np.e/3 * ((np.pi - x**2)**(1/2)) * np.sin(a * np.pi * x))*4
+
+def init():
+    line1.set_data([], [])
+    line2.set_data([], [])
+    return line1,line2
+
+def update(frame):
+    a = frame * 1 
+    y = f(x, a)
+    line1.set_data(-x, y)
+    line2.set_data(x, y)
+    return line1,line2
+
+x = np.linspace(0, np.pi, 1000)
+
+fig, ax = plt.subplots()
+ax.set_xlim(-3, 3)
+ax.set_ylim(-10, 10)
+line1, = ax.plot([], [], color='r')
+line2, = ax.plot([], [], color='r')
+
+plt.title('This is for you!❤')
+
+ani = FuncAnimation(fig, update, frames=range(40), init_func=init, blit=True)
+
+ani.save('heart.mp4', fps=10, extra_args=['-vcodec', 'libx264'])
+
+plt.show()
